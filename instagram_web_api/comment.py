0cc81053d361fe32622deb889f7debd7ddc60507
mouh2020 <badimohammed2019@gmail.com>
2023-06-13 01:47:17 +0100

    add comment

---
+++ instagram_web_api/comment.py
@@ -0,0 +1,10 @@
+
+class Comment : 
+    def comment_to_media(self,comment : str , media_id :str) : 
+        self.session.headers  ['x-csrftoken'] = self.get_cookies["csrftoken"]
+        data = {
+            "comment_text" : comment
+        }
+        return self._make_call(endpoint = f"web/comments/{media_id}/add/",
+                               data=data,
+                               response_type= "comment.comment_to_media")
