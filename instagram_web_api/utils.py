9b995622ba378de8c7782a0425f02a952349541b
mouh2020 <badimohammed2019@gmail.com>
2023-06-13 15:06:51 +0100

    check if a phone number is valid

--- instagram_web_api/utils.py
+++ instagram_web_api/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from .exceptions import *
+import re,phonenumbers
 
 def generate_csrf_token():
     import string,random
@@ -9,6 +10,21 @@ def generate_csrf_token():
 
 def get_id() :
     return int(datetime.now().timestamp())
+
+def is_valid_username(username) : 
+    pattern = r"^[a-zA-Z0-9_.][a-zA-Z0-9_.]*$"
+    if re.match(pattern, username) is None : 
+        raise AccountError("Usernames can only use letters, numbers, underscores and periods.")
+def is_valid_phone_number(phone_number) : 
+    try : 
+        parsed_number = phonenumbers.parse(phone_number)
+        valid_phone_number = phonenumbers.is_possible_number(parsed_number)
+        if not(valid_phone_number) :
+            raise AccountError("Looks like your phone number may be incorrect. Please try entering your full number, including the country code.")
+    except Exception as e : 
+        raise AccountError(str(e))
+
+
 
 def exception_handler(message) : 
     if  "Media type invalid" in message : 
@@ -27,3 +43,10 @@ def exception_handler(message) :
         raise DeletedMedia(str(message))
     elif "Unable to post comment." in message : 
         raise CommentDisabled(message)
+    elif "you've changed it twice within 14 days." in message :
+        raise AccountNameLimit("You can't change your name right now because you've changed it twice within 14 days. You can however remove your name at any time.")
+    elif "You need an email or confirmed phone number." in message : 
+        raise AccountError('You need an email or confirmed phone number.')
+    elif '"errors":[]' in message : 
+        raise AccountError('Unknow error when try change account info.')
+    
